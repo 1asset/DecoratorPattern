8d26afaaba9fc5566698e7931e8ecff04e004517
1asset <82859085+1asset@users.noreply.github.com>
2021-10-05 00:19:15 +0600

    Create Sniper.java

---
+++ com/company/Sniper.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Sniper extends Soldier {
+  
+	public Sniper() {
+		description = "Sniper, with a pistol";
+	}
+  
+	public int number_of_guns() {
+		return 1;
+	}
+}
+
